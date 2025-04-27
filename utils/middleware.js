f581bb6d55fe21e65d4e23e1bcabc97c320947a5
Anjan <anjanarukala@gmail.com>
2025-04-27 19:36:09 +0530

    nothing

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,24 @@
-const { listingSchema, reviewSchema } = require("../schema.js");
+const Joi = require('joi');
 const ExpressError = require("../utils/ExpressError.js");
+
+// Validation for listing
+const listingSchema = Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().allow(''),
+    image: Joi.object({
+        url: Joi.string().allow(''),
+        filename: Joi.string().allow('')
+    }),
+    price: Joi.number().required(),
+    location: Joi.string().allow(''),
+    country: Joi.string().allow(''),
+});
+
+// Validation for review
+const reviewSchema = Joi.object({
+    rating: Joi.number().min(1).max(5).required(),
+    comment: Joi.string().required(),
+});
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
